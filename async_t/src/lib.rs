f8c790b91a66be88a24c1bd94bd722c4c9dbebad
Jose Ernesto Salazar <znx3p0@gmail.com>
2021-11-09 13:54:51 -0700

    renamed

--- async_t/src/lib.rs
+++ async_t/src/lib.rs
@@ -1,6 +1,6 @@
 
 #[rustversion::any(nightly, beta)]
-pub use impl_trait::async_trait;
+pub use async_t_internal::async_trait;
 
 #[rustversion::stable]
 pub use async_trait::async_trait as async_trait;
